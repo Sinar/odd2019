b06c9aae71365e7a59d17c7892cf7e424bb7c2f7
Michael Leow <leow@sinarproject.org>
2019-03-16 20:33:00 +0800

    Summary: Basic pre-req setup for colly

--- scrapers/OSCv3/cmd/oscv3.go
+++ scrapers/OSCv3/cmd/oscv3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 	"github.com/y0ssar1an/q"
@@ -12,11 +13,39 @@ func main() {
 	BasicCollyFromRaw()
 }
 
+func rawDataFolderSetup(absoluteRawDataPath string) {
+	fi, lerr := os.Stat(absoluteRawDataPath)
+	if lerr != nil {
+		if os.IsNotExist(lerr) {
+			// Create the needed folder as per needed .. all along the chain
+			mkerr := os.MkdirAll(absoluteRawDataPath, 0700)
+			if mkerr != nil {
+				panic(mkerr)
+			}
+		} else {
+			// SOme other unknown issue
+			panic(lerr)
+		}
+	} else {
+		// spew.Dump(fi)
+		if !fi.IsDir() {
+			panic(fmt.Errorf("NOT DIR!! %s", absoluteRawDataPath))
+		}
+		fmt.Println("Directory ", absoluteRawDataPath, " EXISTS!!")
+	}
+}
+
 // BasicCollyFromRaw is meant to read data from fixtures and extract out data ..
 func BasicCollyFromRaw() {
 	// From the collection; can run another round while tweaking the strcuture
 	// Removing the extra cost of network and being blocked ..
+	var currentDateLabel = "20190316"
+	var uniqueSearchID = "penang-kulim-0212"
+	// NOTE: Won't work on Windoze :(
+	var absoluteRawDataPath = fmt.Sprintf("raw/%s/%s", currentDateLabel, uniqueSearchID)
+	rawDataFolderSetup(absoluteRawDataPath)
 
+	// With pre-reqs setup; we can proceed ...
 	c := colly.NewCollector(
 		colly.UserAgent("Sinar Project :P"),
 	)
---
+++ scrapers/OSCv3/raw/README.md
@@ -0,0 +1,6 @@
+# Raw scrape files
+
+## Structure
+
+- HTML
+<YYYYMMDD>/<uniq-search-id>/..-<pg-number>.html
