4092078989f36c345f06a0b99028b58a613b2d85
Michael Leow <leow@sinarproject.org>
2019-06-23 21:23:53 +0800

    Summart: Add new data for MBPJ + MY ending June 23, 2019

--- scrapers/OSCv3/cmd/oscv3-find-new.go
+++ scrapers/OSCv3/cmd/oscv3-find-new.go
@@ -310,11 +310,11 @@ func FindNewRequests(authorityToScrape string) {
 	var uniqueSearchID = mapAuthorityToDirectory(authorityToScrape)
 
 	// Refactor  out the currentDate
-	var currentDateLabel = "20190617"
+	var currentDateLabel = "20190623"
 	currentSnapshot := extractDataFromSnapshot(volumePrefix, currentDateLabel, uniqueSearchID)
 	// If in Codefresh; do a branch, git add + commit?
 	// Refactor out the previousDate
-	var previousDateLabel = "20190607"
+	var previousDateLabel = "20190617"
 	fmt.Println("Now compare against the previous: ", previousDateLabel)
 	previousSnapshot := extractDataFromSnapshot(volumePrefix, previousDateLabel, uniqueSearchID)
 
