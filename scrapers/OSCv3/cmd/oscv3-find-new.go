a13d27824996fd66ca36716d3bae7cb3232199f0
Michael Leow <leow@sinarproject.org>
2020-07-18 20:45:04 +0800

    Summary: Data back; updated till July 18, 2020

--- scrapers/OSCv3/cmd/oscv3-find-new.go
+++ scrapers/OSCv3/cmd/oscv3-find-new.go
@@ -310,11 +310,11 @@ func FindNewRequests(authorityToScrape string) {
 	var uniqueSearchID = mapAuthorityToDirectory(authorityToScrape)
 
 	// Refactor  out the currentDate
-	var currentDateLabel = "20200511"
+	var currentDateLabel = "20200718"
 	currentSnapshot := extractDataFromSnapshot(volumePrefix, currentDateLabel, uniqueSearchID)
 	// If in Codefresh; do a branch, git add + commit?
 	// Refactor out the previousDate
-	var previousDateLabel = "20200307"
+	var previousDateLabel = "20200511"
 	fmt.Println("Now compare against the previous: ", previousDateLabel)
 	previousSnapshot := extractDataFromSnapshot(volumePrefix, previousDateLabel, uniqueSearchID)
 
